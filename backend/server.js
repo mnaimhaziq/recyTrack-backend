38a51aee833241bd583181d4e3ed1383c9190c61
mnaimhaziq <101214356+mnaimhaziq@users.noreply.github.com>
2023-04-01 08:45:42 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,6 @@ connectDB();
 const app = express();
 app.use(cors());
 
-// Serve the static files from the React app
-app.use(express.static(path.join(new URL('../build', import.meta.url).pathname)));
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(new URL('../build/index.html', import.meta.url).pathname));
-});
 
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
