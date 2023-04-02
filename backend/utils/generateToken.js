862f95cf4f71fa2aea1d42fc2f618af2e9eab1e1
mnaimhaziq <101214356+mnaimhaziq@users.noreply.github.com>
2023-04-03 03:50:13 +0800

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-// Function to generate a token based on user ID
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-};
-export default generateToken;
